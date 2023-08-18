474ff92a5257f0b48c01f78342c90e6080420446
Alan <alanhaugen@gmail.com>
2023-08-18 12:47:41 +0200

    Trying to rotate the cubes... Very strange it does not work

--- source/piano.cpp
+++ source/piano.cpp
@@ -7,7 +7,9 @@ Piano::Piano()
 
     for (int i = 0; i < WHITE_KEYS_QUANTITY; i++)
     {
-        whiteKeys[i].matrix.Translate(glm::vec3(-10.0f + i, 0.0f, -10.0f));
+        whiteKeys[i].matrix.Translate(glm::vec3(-20.0f + i, -5.0f, -10.0f));
+        whiteKeys[i].matrix.Scale(glm::vec3(1, 1, 3));
+        whiteKeys[i].matrix.Rotate(glm::radians(155.0f), glm::vec3(1,0,0));
     }
 
     for (int i = 0; i < BLACK_KEYS_QUANTITY; i++)
